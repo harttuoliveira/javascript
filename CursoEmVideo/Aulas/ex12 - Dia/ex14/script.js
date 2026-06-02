38ce30b71deb3276a5756eb67ff60b3b2f82c78b
Harttu <harttuoliveira@gmail.com>
2026-06-02 13:54:41 -0300

    Termino Dias

---
+++ CursoEmVideo/Aulas/ex12 - Dia/ex14/script.js
@@ -0,0 +1,26 @@
+function  carregar(params) {
+  var msg = document.getElementById('msg')
+  var img = document.getElementById('imagem')
+  var data = new Date()
+  var hora = data.getHours()
+  //var hora = 2
+  msg.innerHTML = `Agora são ${hora} horas.`
+  if (hora >= 0 && hora < 5) {
+    // BOA MADRUGADA!
+    img.src = 'fotomadruga.jpg'
+    document.body.style.background = 'var(--madruga)'
+  } else if (hora >= 5 && hora < 12) {
+    // BOM DIA!
+    img.src = 'fotomanha.jpg'
+    document.body.style.background = 'var(--manha)'
+  } else if (hora >= 12 && hora <= 18) {
+    // BOA TARDE!
+    img.src = 'fototarde.jpg'
+    document.body.style.background = 'var(--tarde)'
+  } else if (hora >= 19 && hora <= 23) {
+    // BOA NOITE!
+    img.src = 'fotonoite.jpg'
+    document.body.style.background = 'var(--noite)'
+  }
+}
+
